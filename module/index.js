d6614f370afa1da716052351c79e5cf3596f5988
ftourangeau <francis.tourangeau@hrsdc-rhdcc.gc.ca>
2022-03-02 15:45:30 -0500

    added prompt for filling login.json

--- module/index.js
+++ module/index.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const auth = require('./settings/login.json');
 const axios = require("axios");
 axios.defaults.withCredentials = true;
 var fp = require("find-free-port");
@@ -9,74 +8,102 @@ var os = require('os');
 var networkInterfaces = os.networkInterfaces();
 var firstNetworkConnection = Object.keys(networkInterfaces)[0];
 
-const port = process.env.PORT || 5000;
+var prompt = require('prompt');
 
-app.use(express.static('assets'));
-app.use(cors());
+const fs = require('fs')
 
-app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/assets/index.html");
-});
+const path = __dirname + '/settings/login.json'
 
-app.get("/courses", (req, res) => {
-    res.sendFile(__dirname + "/assets/courses/index.html");
-});
 
-app.get("/courses/list", (req, res) => {
-          axios.post('https://esdcadapt.canadacentral.cloudapp.azure.com/api/login', {
-            email: auth.email,
-            password: auth.password
-          })
-          .then(function (response) {
-            var cookies = response.headers['set-cookie'];
-            axios.get('https://esdcadapt.canadacentral.cloudapp.azure.com/api/content/course/query', {
-                headers: {Cookie: cookies}}).then(response => {
-                    res.send(response.data);
-                })
-          })
-          .catch(function (error) {
-            //console.log(error);
-            var status = error.response.statusText;
-            if(status === 'Unauthorized'){
-              console.log('The request cannot be authorized, make sure you have valid credentials in module/settings/login.json')
-            }
-            res.status(401).send(error);
-          });
-});
+  if (fs.existsSync(path)) {
+    deploy();
+  } else {
+    console.log('This is your first time running the tool! Plese input valid Adapt credentials in order to create the settings file.')
+    prompt.message = 'Adapt';
+    prompt.start();
+    prompt.get(['email', 'password'], function (err, result) {
+      var json = {
+        email: result.email,
+        password: result.password
+      }
+      fs.writeFileSync(__dirname + '/settings/login.json', JSON.stringify(json));
+      deploy();
+    });
+  }
 
-app.get("/users", (req, res) => {
-  res.sendFile(__dirname + "/assets/users/index.html");
-});
 
-app.get("/users/list", (req, res) => {
-  axios.post('https://esdcadapt.canadacentral.cloudapp.azure.com/api/login', {
-    email: auth.email,
-    password: auth.password
-  })
-  .then(function (response) {
-    var cookies = response.headers['set-cookie'];
-    axios.get('https://esdcadapt.canadacentral.cloudapp.azure.com/api/user', {
-        headers: {Cookie: cookies}}).then(response => {
-            res.send(response.data);
-        })
-  })
-  .catch(function (error) {
-    var status = error.response.statusText;
-    if(status === 'Unauthorized'){
-      console.log('The request cannot be authorized, make sure you have valid credentials in module/settings/login.json')
-    }
-    res.status(401).send(error);
-  });
-});
 
-fp(8080, 9000, function(err, freePort) {
+function deploy(){
 
-  var PORT = freePort;
+  const auth = require('./settings/login.json');
 
-  app.listen(PORT, () => {
-    console.log('Server available on http://localhost:' + PORT);
-    console.log(networkInterfaces[firstNetworkConnection][1].address + ':' + PORT);
+  app.use(express.static('assets'));
+  app.use(cors());
+
+  app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/assets/index.html");
   });
 
-});
+  app.get("/courses", (req, res) => {
+      res.sendFile(__dirname + "/assets/courses/index.html");
+  });
 
+  app.get("/courses/list", (req, res) => {
+            axios.post('https://esdcadapt.canadacentral.cloudapp.azure.com/api/login', {
+              email: auth.email,
+              password: auth.password
+            })
+            .then(function (response) {
+              var cookies = response.headers['set-cookie'];
+              axios.get('https://esdcadapt.canadacentral.cloudapp.azure.com/api/content/course/query', {
+                  headers: {Cookie: cookies}}).then(response => {
+                      res.send(response.data);
+                  })
+            })
+            .catch(function (error) {
+              //console.log(error);
+              var status = error.response.statusText;
+              if(status === 'Unauthorized'){
+                console.log('The request cannot be authorized, make sure you have valid credentials in module/settings/login.json')
+              }
+              res.status(401).send(error);
+            });
+  });
+
+  app.get("/users", (req, res) => {
+    res.sendFile(__dirname + "/assets/users/index.html");
+  });
+
+  app.get("/users/list", (req, res) => {
+    axios.post('https://esdcadapt.canadacentral.cloudapp.azure.com/api/login', {
+      email: auth.email,
+      password: auth.password
+    })
+    .then(function (response) {
+      var cookies = response.headers['set-cookie'];
+      axios.get('https://esdcadapt.canadacentral.cloudapp.azure.com/api/user', {
+          headers: {Cookie: cookies}}).then(response => {
+              res.send(response.data);
+          })
+    })
+    .catch(function (error) {
+      var status = error.response.statusText;
+      if(status === 'Unauthorized'){
+        console.log('The request cannot be authorized, make sure you have valid credentials in module/settings/login.json')
+      }
+      res.status(401).send(error);
+    });
+  });
+
+  fp(8080, 9000, function(err, freePort) {
+
+    var PORT = freePort;
+
+    app.listen(PORT, () => {
+      console.log('Server available on http://localhost:' + PORT);
+      console.log(networkInterfaces[firstNetworkConnection][1].address + ':' + PORT);
+    });
+
+  });
+}
+ 
